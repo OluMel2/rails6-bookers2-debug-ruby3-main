5040a73840dd0ca50145f9d130d38e18d6b05710
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-05-13 12:26:40 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,27 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back fallback_location: root_path
+    #redirect_back fallback_location: root_path
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    #redirect_to book_path(params[:book_id])
   end
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_comment = BookComment.new
+  end
+
 
 end
